203347e394709956086d820a07b64c38b855eafd
Denys Matusevych <denytop3@gmail.com>
2026-07-10 14:40:56 +0300

    add tag CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from todo.views import TagListView, TaskListView
+from todo.views import (
+    TagListView,
+    TaskListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home-page"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="tag-delete"),
 ]
